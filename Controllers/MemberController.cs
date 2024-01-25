95e41d1591883c3bf8e266a40712ba5698c09cbe
WBCOHBO <andy405641@gmail.com>
2024-01-25 17:03:18 +0800

    1. 增加購物車的新增與刪除功能 2. 訂單收件人資料尚未完成OrderList()方法

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -32,5 +32,73 @@ namespace DemoShoppingWebsite.Controllers
             var orderDetails = db.table_OrderDetail.Where(m => m.UserId == UserId && m.IsApproved == "否").ToList();
             return View(orderDetails);
         }
+
+        public ActionResult AddCar(string ProductId)
+        {
+            //取得目前通過驗證的使用者名稱
+            string userId = User.Identity.Name;
+
+            //取得該使用者目前購物車內是否已有此商品，且尚未形成訂單的資料
+            var currentCar = db.table_OrderDetail
+                .Where(m => m.ProductId == ProductId && m.IsApproved == "否" && m.UserId == userId).FirstOrDefault();
+            if (currentCar == null)
+            {
+                //如果篩選條件資料為null，代表要新增全新一筆訂單明細資料
+                //將產品資料欄位一一對照至訂單明細的欄位
+                var product = db.table_Product.Where(m => m.ProductId == ProductId).FirstOrDefault();
+                var orderDetail = new table_OrderDetail();
+                orderDetail.UserId = userId;
+                orderDetail.ProductId = product.ProductId;
+                orderDetail.Name = product.Name;
+                orderDetail.Price = product.Price;
+                orderDetail.Quantity = 1;
+                orderDetail.IsApproved = "否";
+                db.table_OrderDetail.Add(orderDetail);
+            }
+            else
+            {
+                //如果購物車已有此商品，僅需將數量加1
+                currentCar.Quantity++;
+            }
+
+            //儲存資料庫並導至購物車檢視頁面
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        public ActionResult DeleteCar(int Id)
+        {
+            var orderDetails = db.table_OrderDetail.Where(m => m.Id == Id).FirstOrDefault();
+            db.table_OrderDetail.Remove(orderDetails);
+            db.SaveChanges();
+            return RedirectToAction("ShoppingCar");
+        }
+
+        [HttpPost]
+        public ActionResult ShoppingCar(string Receiver, string Email, string Address)
+        {
+            string userId = User.Identity.Name;
+            string guid = Guid.NewGuid().ToString(); //產生隨機訂單編號
+
+            //加入訂單至 table_Order 資料表
+            var order = new table_Order();
+            order.OrderGuid = guid;
+            order.UserId = userId;
+            order.Receiver = Receiver;
+            order.Email = Email;
+            order.Address = Address;
+            order.Date = DateTime.Now;
+            db.table_Order.Add(order);
+
+            //訂單加入後，需一併更新訂單明細內容
+            var carList = db.table_OrderDetail.Where(m => m.IsApproved == "否" && m.UserId == userId).ToList();
+            foreach(var item in carList)
+            {
+                item.OrderGuid = guid;
+                item.IsApproved = "是";
+            }
+            db.SaveChanges();
+            return RedirectToAction("OrderList");
+        }
     }
 }
