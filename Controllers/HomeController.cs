75d40983f54406f87e3bcd70cf1ec31796b1270a
WBCOHBO <andy405641@gmail.com>
2024-01-25 11:15:46 +0800

    1. 新增Register頁面與功能 2. 將預設沒用到的view刪除

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoShoppingWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,22 +9,36 @@ namespace DemoShoppingWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        dbShoppingCarEntities db = new dbShoppingCarEntities();
         public ActionResult Index()
         {
+            var products = db.table_Product.OrderByDescending(m =>  m.Id).ToList();
+            return View(products);
+        }
+
+        public ActionResult Register()
+        {
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Register(table_Member Member)
         {
-            ViewBag.Message = "Your application description page.";
+            //如果資料驗證未通過，則回傳原本的View
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            //如果註冊帳號不存在，才能新增與儲存
+            var member = db.table_Member.Where(m => m.UserId == Member.UserId).FirstOrDefault();
+            if(member == null)
+            {
+                db.table_Member.Add(Member);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            ViewBag.Message = "帳號已被使用，請重新註冊";
             return View();
         }
     }
